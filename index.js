590869753fbb7fb91f8a9ed8cc42989d9d2bf1b1
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2024-01-21 18:03:21 -0500

    did a few more changes

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateSvg = require('./utils/generateSvg');
+const inquirer = require("inquirer");
+const Svg = require("./lib/svg");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+const fs = require("fs");
 
 const questions = [
   {
-    type: 'input',
-    name: 'text',
-    message: 'Enter up to three characters for the text:',
-    validate: (input) => input.length <= 3, 
+    type: "input",
+    name: "text",
+    message: "Enter up to three characters:",
+    validate: (input) => input.length <= 3,
   },
   {
-    type: 'input',
-    name: 'textColor',
-    message: 'Enter text color (keyword or hexadecimal):',
+    type: "input",
+    name: "textColor",
+    message: "Enter text color (color keyword or hexadecimal number):",
   },
   {
-    type: 'list',
-    name: 'shape',
-    message: 'Choose a shape:',
-    choices: ['circle', 'triangle', 'square'],
+    type: "list",
+    name: "shape",
+    message: "Choose a shape:",
+    choices: ["circle", "triangle", "square"],
   },
   {
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Enter shape color (keyword or hexadecimal):',
+    type: "input",
+    name: "shapeColor",
+    message: "Enter shape color (color keyword or hexadecimal number):",
   },
 ];
 
-const createSVG = (fileName, data) => {
-  const template = generateSvg(data);
-  fs.writeFile(fileName, template, (err) => {
-    err ? console.error(err) : console.log('Generated logo.svg');
+inquirer
+  .prompt(questions)
+  .then((response) => {
+    let shape;
+    if (response.shape === "circle") {
+      shape = new Circle();
+    } else if (response.shape === "triangle") {
+      shape = new Triangle();
+    } else {
+      shape = new Square();
+    }
+    shape.setColor(response.shapeColor);
+    const svg = new Svg();
+    svg.setShape(shape);
+    svg.setText(response.text, response.textColor);
+    fs.writeFile(`examples/logo-${response.text}.svg`, svg.render(), (err) => {
+      if (err) {
+        console.error(`Error writing file: ${err.message}`);
+        // Handle the error, e.g., throw err; or return
+      }
+      console.log("Your logo was created!!");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
   });
-};
-
-const init = () => {
-  inquirer.prompt(questions).then((data) => {
-    createSVG('./output/logo.svg', data);
-  });
-};
-
-init();
